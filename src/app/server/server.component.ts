280c3429cc7126ae061b4447b9efa9008c1e95cc
theashishmalik <imashishmalik.45@gmail.com>
2019-05-19 02:37:37 +0530

    dynamic variable

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector:'app-server',
-    templateUrl:'./server.component.html',
- 
+    selector: 'app-server',
+    templateUrl: './server.component.html',
+
 }) //to separate from normal classes add decorator in component
 export class ServerComponent {
+    serverid = 15;
+    serverStatus = 'online'
+    allowServer = false;
+
+    constructor() {
+
+        setTimeout(() => {
+            this.allowServer = true;
+        }, 2000)
+
+    }
+
 
 }
